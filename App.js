93a185659c1760acae16ca88e0bb3ec9d28768fb
johannes <jmuljana@gmail.com>
2022-10-12 19:16:51 +1100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button, TouchableHighlight } from 'react-native';
+import Constants from 'expo-constants';
+
+const ListData = [
+  { name: "item 1", id: "1", completed: false },
+  { name: "item 2", id: "2", completed: false },
+  { name: "item 3", id: "3", completed: false },
+]
 
 export default function App() {
+  const renderer = ({item}) => (
+    <View>
+      <Text>{ item.name } </Text>
+      <Text>id: {item.id} </Text>
+    </View>
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputGroup} >
+        <TextInput style={styles.input} placeholder='add an item'/>
+        <TouchableHighlight>
+          <Text>Add</Text>
+        </TouchableHighlight>
+      </View>
+      <FlatList 
+        data = {ListData}
+        renderItem = { renderer }
+      />
     </View>
   );
 }
@@ -14,7 +36,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    justifyContent: 'start',
+  },
+  inputGroup: {
+    flexDirection: "row",
+  },
+  input {
+    flex: 1,
   },
 });
